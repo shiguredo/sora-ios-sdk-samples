a01148ce3d66a56a7b7752300ec5d33aa77efb2a
SUZUKI Tetsuya <szktty@shiguredo.jp>
2020-01-14 16:59:14 +0900

    2020.1 に対応する

--- DecoStreamingSample/DecoStreamingSample/Classes/PublisherConfigViewController.swift
+++ DecoStreamingSample/DecoStreamingSample/Classes/PublisherConfigViewController.swift
@@ -40,12 +40,13 @@ class PublisherConfigViewController: UITableViewController {
         }
         
         // 入力された設定を元にSoraへ接続を行います。
-        // この画面からは配信側に接続を行うため、role引数には .publisher を指定しています。
+        // この画面からは配信側に接続を行うため、role引数には .sendonly を指定しています。
         // また今回のサンプルアプリでは、デフォルトのカメラ映像のキャプチャではなく、加工されたカメラ映像のキャプチャを使用したいため、
         // videoCapturerOptionをカスタムに設定しておきます。
         SoraSDKManager.shared.connect(
             channelId: channelId,
-            role: .publisher,
+            role: .sendonly,
+            multistreamEnabled: false,
             videoCodec: videoCodec,
             videoCapturerOption: .custom
         ) { [weak self] error in
