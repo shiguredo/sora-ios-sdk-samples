4275cbce01a1cd32158b255773c47f531fd2c8fb
miosakuma <mio.sakuma.gml@gmail.com>
2023-07-31 19:16:46 +0900

    変数名をほかのサンプルと合わせる

--- DataChannelSample/DataChannelSample/ConfigViewController.swift
+++ DataChannelSample/DataChannelSample/ConfigViewController.swift
@@ -193,7 +193,7 @@ class ConfigViewController: UITableViewController {
         // 入力された設定を元にSoraへ接続を行います。
         // ビデオチャットアプリでは複数のユーザーが同時に配信を行う必要があるため、
         // role 引数には .sendrecv を指定し、マルチストリームを有効にします。
-        var configuration = Configuration(urlCandidates: Environment.urlCandidates, channelId: channelId, role: role, multistreamEnabled: multistreamEnabledSwitch.isOn)
+        var configuration = Configuration(urlCandidates: Environment.urls, channelId: channelId, role: role, multistreamEnabled: multistreamEnabledSwitch.isOn)
         configuration.videoEnabled = videoEnabledSwitch.isOn
         configuration.videoCodec = videoCodec
         configuration.audioEnabled = audioEnabledSwitch.isOn
