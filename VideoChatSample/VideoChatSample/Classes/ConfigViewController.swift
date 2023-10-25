2b3cda4b1a1833a358f27209577259964dfd4a23
miosakuma <mio.sakuma.gml@gmail.com>
2023-10-25 18:00:58 +0900

    lint-format.sh の結果を反映する

--- VideoChatSample/VideoChatSample/Classes/ConfigViewController.swift
+++ VideoChatSample/VideoChatSample/Classes/ConfigViewController.swift
@@ -117,7 +117,7 @@ class ConfigViewController: UITableViewController {
 
         let videoH264Params = h264ProfileLevelId != nil ? ["profile_level_id": h264ProfileLevelId!] : nil
         configuration.videoH264Params = videoH264Params
-        
+
         configuration.signalingConnectMetadata = Environment.signalingConnectMetadata
 
         // 入力された設定を元にSoraへ接続を行います。
