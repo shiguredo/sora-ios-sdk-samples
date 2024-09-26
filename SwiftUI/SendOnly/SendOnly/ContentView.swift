50d5ff5ffac7b2f9db347c7e541288d61be8ad66
zztkm <zztkm@tsurutatakumi.info>
2024-09-26 15:01:41 +0900

    初期化

---
+++ SwiftUI/SendOnly/SendOnly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SendOnly
+//
+//  Created by zztkm on 2024/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI/SendOnly/SendOnly/SendOnlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendOnlyApp.swift
+//  SendOnly
+//
+//  Created by zztkm on 2024/09/26.
+//
+
+import SwiftUI
+
+@main
+struct SendOnlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
