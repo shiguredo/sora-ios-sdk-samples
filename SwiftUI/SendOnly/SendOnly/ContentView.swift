c7881fcab0c3dcf78575ec79831231248118ff1c
zztkm <zztkm@tsurutatakumi.info>
2024-09-26 19:34:08 +0900

    SoraSDKManager を削除

--- SwiftUI/SendOnly/SendOnly/ContentView.swift
+++ SwiftUI/SendOnly/SendOnly/ContentView.swift
@@ -2,14 +2,16 @@ import SwiftUI
 import Sora
 
 struct ContentView: View {
+    @State private var mediaChannel: MediaChannel?
+
     @State private var senderStream: MediaStream?
     // アラートを表示するための状態管理変数
     @State private var showsAlert = false
     // 接続時のエラー
     @State private var connectionError: Error?
-
-    var mediaChannel: MediaChannel? {
-        SoraSDKManager.shared.currentMediaChannel
+    // 接続状態
+    private var isConnected: Bool {
+        mediaChannel?.isAvailable ?? false
     }
 
     var body: some View {
@@ -26,13 +28,13 @@ struct ContentView: View {
                         } label: {
                             Text("接続")
                                 .font(.title)
-                        }
+                        }.disabled(isConnected)
                         Button {
                             disconnect()
                         } label: {
                             Text("切断")
                                 .font(.title)
-                        }
+                        }.disabled(!isConnected)
                     }
                 }.padding()
 
@@ -54,13 +56,18 @@ struct ContentView: View {
     }
 
     func connect() {
+        if isConnected {
+            print("Already connected.")
+            return
+        }
+
         var config = Configuration(
             urlCandidates: AppEnvironment.urls,
             channelId: AppEnvironment.channelId,
             role: .sendonly,
             multistreamEnabled: true)
         config.signalingConnectMetadata = AppEnvironment.signalingConnectMetadata
-        
+
         // mediaChannel のイベントハンドラ
         config.mediaChannelHandlers.onConnect = { error in
             if error != nil {
@@ -71,22 +78,31 @@ struct ContentView: View {
             // 接続に成功した場合、senderStream がセットされているので senderStream にセットする
             self.senderStream = self.mediaChannel?.senderStream
         }
-        
-        // Sora 接続処理
-        SoraSDKManager.shared.connect(config: config) { error in
-            if let error {
-                self.connectionError = error
-                self.showsAlert = true
+
+        // Soraに接続を試みます。
+        _ = Sora.shared.connect(configuration: config) { mediaChannel, error in
+            // 接続に成功した場合は、mediaChannelに値が返され、errorがnilになります。
+            // 一方、接続に失敗した場合は、mediaChannelはnilとなり、errorが返されます。
+            if let error = error {
+                connectionError = error
+                showsAlert = true
                 print("Error: \(error)")
-            } else {
-                print("Connected")
             }
+            self.mediaChannel = mediaChannel
         }
     }
 
     func disconnect() {
-        // ここで self.senderStream = nil しなくても、disconnet() した時点で nil になる
-        SoraSDKManager.shared.disconnect()
+        if isConnected == false {
+            print("Already disconnected.")
+            return
+        }
+        guard let mediaChannel = self.mediaChannel else {
+            return
+        }
+        mediaChannel.disconnect(error: nil)
+        self.mediaChannel = nil
+        self.senderStream = nil
     }
 }
 
