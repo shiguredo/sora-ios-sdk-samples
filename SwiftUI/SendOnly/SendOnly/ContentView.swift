2dd24886832fe0a260d4505e59a1ca4ccaf62ea5
zztkm <zztkm@tsurutatakumi.info>
2024-09-30 12:41:52 +0900

    SwiftUIVideoView への rename 対応

--- SwiftUI/SendOnly/SendOnly/ContentView.swift
+++ SwiftUI/SendOnly/SendOnly/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             ZStack {
-                Video(senderStream)
+                SwiftUIVideoView(senderStream)
                     .videoAspect(.fill)
                     .ignoresSafeArea()
                 VStack {
